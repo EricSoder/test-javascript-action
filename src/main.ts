750aa7067b059280f43d65fd233a6792675a4cb5
Eric Söderqvist <eric.soderqvist@solidify.dev>
2024-02-26 10:41:15 +0100

    init

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+async function run() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput("who-to-greet");
+    console.log(`Hello ${nameToGreet}!`);
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log(error.message);
+    } else {
+      console.log("An error occurred", error);
+    }
+  }
+}
+
+run();
